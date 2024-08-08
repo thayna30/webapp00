9a14004e12ba3faeda8fe61f6ad72e58826342b5
thayna30 <santos.thaynac@gmail.com>
2024-08-08 09:01:41 -0300

    Update setup.sh
    mudei o email

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 mkdir -p ~/.streamlit/
 echo "\
 [general]\n\
-email = \"prof.massaki@gmail.com\"\n\
+email = \"santos.thaynac@gmail.com\"\n\
 " > ~/.streamlit/credentials.toml
 echo "\
 [server]\n\
